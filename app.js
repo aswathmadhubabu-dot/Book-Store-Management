232dc274e87c96b6ea3b939dd274d85acdd1b086
ases573 <ases573@gmail.com>
2017-06-05 14:29:24 +0530

    routing basics

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app = express();
 
 var PORT = 4000;
 
+app.get("/",function(request,response)
+{
+response.send("Hello world");
+});
+
+app.get("/books",function(request,response)
+{
+response.send("Hello books");
+});
+
 app.listen(PORT,function (err) {
 	console.log("server running at PORT : "+PORT);
 });
