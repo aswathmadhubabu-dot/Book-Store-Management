4f1fc360aaf862b61ec6967e609807e322025114
ases573 <ases573@gmail.com>
2017-06-19 20:23:34 +0530

    passport db insert

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,26 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
-var jsFiles = ['*.js','src/**/*.js'];
+var jsFiles = ['*.js', 'src/**/*.js'];
 
-gulp.task('style',function() {
+gulp.task('style', function() {
     return gulp.src(jsFiles)
-     .pipe(jshint())
-     .pipe(jshint.reporter('jshint-stylish',{
-        verbose : true
-    }))
-     .pipe(jscs());
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
 });
 
-gulp.task('inject',function() {
+gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
-        ignorePath : '../../public'
+        ignorePath: '../../public'
     };
     return gulp.src('./src/views/*.html')
-    .pipe(wiredep(options))
-    .pipe(gulp.dest('./src/views'));
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
 
 });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,15 +5,28 @@ var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient();
 
 authRouter.route('/signUp').post(function(req, res) {
-	console.log(req.body);
-	req.login(req.body, function() {
-		res.redirect('/auth/profile');
+	console.log(req.body.username);
+	var url = 'mongodb://localhost:27017/libraryApp';
+
+	mongodb.connect(url, function(err, db) {
+		var collection = db.collection('users');
+		var user = {
+			username: req.body.username,
+			password: req.body.password
+		};
+		collection.insert(user, function(err, results) {
+			req.login(results.ops[0], function() {
+				res.redirect('/auth/profile');
+			});
+		});
 	});
+
+
 });
 
 
 authRouter.route('/profile').get(function(req, res) {
 	console.log(req.body);
 	res.json(req.user);
-});	
+});
 module.exports = authRouter;
