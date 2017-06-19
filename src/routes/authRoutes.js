75e831e2ddb8447f22c9f89da3a7ba9b6b6a73e9
ases573 <ases573@gmail.com>
2017-06-19 19:42:00 +0530

    passport json login'

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,14 @@ var mongodb = require('mongodb').MongoClient();
 
 authRouter.route('/signUp').post(function(req, res) {
 	console.log(req.body);
+	req.login(req.body, function() {
+		res.redirect('/auth/profile');
+	});
 });
 
+
+authRouter.route('/profile').get(function(req, res) {
+	console.log(req.body);
+	res.json(req.user);
+});	
 module.exports = authRouter;
