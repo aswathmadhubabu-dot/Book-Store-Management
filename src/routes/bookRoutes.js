e582d9f4a6a9d1f9acc6e796a2fa017f6c2dbc41
ases573 <ases573@gmail.com>
2017-06-20 15:12:09 +0530

    "[GIT AUTO COMMIT]: Tue Jun 20 2017 15:12:09 GMT+0530 (India Standard Time)"

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,13 +3,14 @@ var sql = require('mssql');
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 var bookRouter = express.Router();
-var bookController = require('../controllers/bookController')(null);
+
 bookRouter.use(function(req, res, next) {
     if (!req.user) {
         res.redirect('/');
     }
     next();
 });
-bookRouter.route('/').get(bookController.getIndex(null));
-bookRouter.route('/:id').get(bookController.getById(null));
+var bookController = require('../controllers/bookController')(null);
+bookRouter.route('/').get(bookController.getIndex());
+bookRouter.route('/:id').get(bookController.getById());
 module.exports = bookRouter;
