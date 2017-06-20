3b162f7de4a3e0c2cb8b9093f03de14c8120ceb8
ases573 <ases573@gmail.com>
2017-06-20 15:02:09 +0530

    "[GIT AUTO COMMIT]: Tue Jun 20 2017 15:02:09 GMT+0530 (India Standard Time)"

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,20 +1,44 @@
-var bookController = function(bookService){
-
-  var getIndex  = function(req, res) {
-    var url = 'mongodb://localhost:27017/libraryApp';
-    mongodb.connect(url, function(err, db) {
-        var collection = db.collection('books');
-        collection.find({}).toArray(function(err, results) {
-            res.render('book', {
-                title: 'Book',
-                nav: [{
-                    Link: '/authors',
-                    Text: 'Authors'
-                }],
-                Books: results
-            });
-        });
-    });
-}
-  var getbyId 
+var bookController = function(bookService) {
+	var getIndex = function(req, res) {
+		var url = 'mongodb://localhost:27017/libraryApp';
+		mongodb.connect(url, function(err, db) {
+			var collection = db.collection('books');
+			collection.find({}).toArray(function(err, results) {
+				res.render('book', {
+					title: 'Book',
+					nav: [{
+						Link: '/authors',
+						Text: 'Authors'
+					}],
+					Books: results
+				});
+			});
+		});
+	}
+	var getById = function(req, res) {
+		var id = new objectId(req.params.id);
+		var url = 'mongodb://localhost:27017/libraryApp';
+		mongodb.connect(url, function(err, db) {
+			var collection = db.collection('books');
+			collection.findOne({
+					_id: id
+				},
+				function(err, results) {
+					console.log(results);
+					res.render('bookView', {
+						title: 'Book',
+						nav: [{
+							Link: '/authors',
+							Text: 'Authors'
+						}],
+						Book: results
+					});
+				}
+			);
+		});
+	}
+	return {
+		getIndex: getIndex,
+		getById: getById
+	}
 }
