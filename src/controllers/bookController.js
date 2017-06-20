c43aa01f2e3385735367f7b48d012bcf7b9331da
ases573 <ases573@gmail.com>
2017-06-20 15:00:09 +0530

    "[GIT AUTO COMMIT]: Tue Jun 20 2017 15:00:09 GMT+0530 (India Standard Time)"

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,1 +1,20 @@
-var bookController = 
+var bookController = function(bookService){
+
+  var getIndex  = function(req, res) {
+    var url = 'mongodb://localhost:27017/libraryApp';
+    mongodb.connect(url, function(err, db) {
+        var collection = db.collection('books');
+        collection.find({}).toArray(function(err, results) {
+            res.render('book', {
+                title: 'Book',
+                nav: [{
+                    Link: '/authors',
+                    Text: 'Authors'
+                }],
+                Books: results
+            });
+        });
+    });
+}
+  var getbyId 
+}
