e91d16851c344bcefcdffa2904e1728dc5f720e6
ases573 <ases573@gmail.com>
2017-06-20 12:23:16 +0530

    "[GIT AUTO COMMIT]: Tue Jun 20 2017 12:23:16 GMT+0530 (India Standard Time)"

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,13 @@ var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient();
 var passport = require('passport');
 var bodyParser = require('body-parser');
+
+authRouter.use(function(req, res, next) {
+    if (!req.user) {
+        res.redirect('/');
+    }
+    next();
+});
 authRouter.route('/signUp').post(function(req, res) {
 	console.log(req.body.username);
 	var url = 'mongodb://localhost:27017/libraryApp';
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,51 +1,16 @@
 var express = require('express');
-
 var sql = require('mssql');
-
 var mongodb = require('mongodb').MongoClient;
-
 var objectId = require('mongodb').ObjectID;
-
 var bookRouter = express.Router();
-
-var books = [{
-    "author": "Haruki Murakami",
-    "genre": "Fiction",
-    "date": "2006-01-03",
-    "isbn": "1400079276",
-    "price": 13.45,
-    "title": "Kafka on the Shore",
-    "read": false
-}, {
-    "author": "Charles Bukowski",
-    "genre": "Fiction",
-    "date": "2004-07-08",
-    "isbn": "1852272007",
-    "price": 22.48,
-    "title": "Pulp",
-    "read": false
-}, {
-    "author": "Haruki Murakami",
-    "genre": "Fiction",
-    "date": "2002-04-09",
-    "isbn": "037571894X",
-    "price": 9.01,
-    "title": "A Wild Sheep Chase: A Novel",
-    "read": false
-}, {
-    "author": "George Crile",
-    "genre": "History",
-    "date": "2007-11-06",
-    "isbn": "0802143415",
-    "price": 10.5,
-    "title": "Charlie Wilson's War",
-    "read": true
-}];
-
-
+bookRouter.use(function(req, res, next) {
+    if (!req.user) {
+        res.redirect('/');
+    }
+    next();
+});
 bookRouter.route('/').get(function(req, res) {
     var url = 'mongodb://localhost:27017/libraryApp';
-
     mongodb.connect(url, function(err, db) {
         var collection = db.collection('books');
         collection.find({}).toArray(function(err, results) {
@@ -57,18 +22,12 @@ bookRouter.route('/').get(function(req, res) {
                 }],
                 Books: results
             });
-
         });
     });
-
-
-
 });
-
 bookRouter.route('/:id').get(function(req, res) {
     var id = new objectId(req.params.id);
     var url = 'mongodb://localhost:27017/libraryApp';
-
     mongodb.connect(url, function(err, db) {
         var collection = db.collection('books');
         collection.findOne({
@@ -84,14 +43,8 @@ bookRouter.route('/:id').get(function(req, res) {
                     }],
                     Book: results
                 });
-
-
             }
-
         );
-
     });
 });
-
-
 module.exports = bookRouter;
