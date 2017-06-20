28e9a05311dcd1ba22b5b52b645c4481850b1e15
ases573 <ases573@gmail.com>
2017-06-20 11:52:36 +0530

    "[GIT AUTO COMMIT]: Tue Jun 20 2017 11:52:36 GMT+0530 (India Standard Time)"

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,11 +1,17 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var mongodb = require('mongodb').MongoClient();
 
 module.exports = function() {
 	passport.use(new LocalStrategy({
 		'usernameField': 'username',
 		'passwordField': 'password'
 	}, function(username, password, done) {
+		var url = 'mongodb://localhost:27017/libraryApp';
+
+	mongodb.connect(url, function(err, db) {
+		var collection = db.collection('users');
+		});
 		var user = {
 			username: username,	
 			password: password
